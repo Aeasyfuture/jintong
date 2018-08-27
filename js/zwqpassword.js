ad24f1466d51b7fd2f192b9539df4bad1c875f34
unknown <zzzwwq>
2018-08-28 00:16:02 +0800

    zwq

---
+++ js/zwqpassword.js
@@ -0,0 +1,28 @@
+window.onload=function () {
+/*
+    let circle2=document.querySelectorAll(".circle2");
+    circle2.forEach(function(v,i){
+        circle2[i].onclick=function(){
+            circle2.forEach(function(v){
+                v.classList.remove("circle4");
+            })
+            circle2[i].classList.add("circle4");
+        }
+    })
+*/
+/*    let point=document.querySelectorAll(".tanchukuang ul li");
+    let point1=document.querySelectorAll(".tanchukuang ul li .point");
+    point.forEach(function(v,i){
+        point[i].onclick=function(){
+            point1.forEach(function(va){
+                va.style.background="";
+                va.style.border="";
+                v.style.background="";
+            })
+            point[i].style.background="#2fa5ff";
+            point1[i].style.background="#fff";
+            point1[i].style.border="0.05rem solid #1c8adf;";
+        }
+    })*/
+
+}
---
+++ js/zwqpayment.js
@@ -0,0 +1,12 @@
+window.onload=function () {
+    let circle2=document.querySelectorAll(".circle2");
+    circle2.forEach(function(v,i){
+        circle2[i].onclick=function(){
+            circle2.forEach(function(v){
+                v.classList.remove("circle4");
+            })
+            circle2[i].classList.add("circle4");
+        }
+    })
+
+}
