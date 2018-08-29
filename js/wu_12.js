a8d06689298c7d71b7a04fef160886c6ee422345
wujinsen456 <1098206859@qq.com>
2018-08-29 19:46:56 +0800

    frist

---
+++ js/wu_12.js
@@ -0,0 +1,6 @@
+window.onload=function () {
+    let Return=document.querySelector(".icon-taobaozhongchou4qizuoyoujiantou1");
+    Return.onclick=function () {
+        open("index_wu5.html","")
+    }
+}
